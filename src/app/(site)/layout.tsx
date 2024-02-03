386d51ef5fd5d315b37f8781ac4b33b4d3eccf2d
izuchukwu-eric <onukwubeizu@gmail.com>
2024-02-03 17:34:53 +0100

    landing page: header, body, footer

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/landing-page/footer'
+import Header from '@/components/landing-page/header'
+import React from 'react'
+
+const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <Header />
+      {children}
+      <Footer />
+    </main>
+  )
+}
+
+export default HomePageLayout
